c0561e6539b7b0eb0a1f5e11f491088f43b0335f
ethan-x11 <subhamdutta96740@gmail.com>
2023-04-15 01:50:44 +0530

    database module

--- data.py
+++ data.py
@@ -1,6 +1,8 @@
 import pymongo
 import json
 import pandas as pd
+import os
+from dotenv import load_dotenv
 
 def remove_id(obj):
     if isinstance(obj, list):
@@ -11,7 +13,13 @@ def remove_id(obj):
         return obj
 
 def get_data():
-    client = pymongo.MongoClient("mongodb+srv://salary_data_cluster_11401:TXxUcL2fGSRcifw5@salarydatacluster.scpzbio.mongodb.net/test?retryWrites=true&w=majority")
+    load_dotenv()  # Load environment variables from .env file
+
+    DB_NAME = os.getenv('DB_NAME')
+    DB_USER = os.getenv('DB_USER')
+    DB_PASSWORD = os.getenv('DB_PASSWORD')
+    DB_HOST = os.getenv('DB_HOST')
+    client = pymongo.MongoClient(f"mongodb+srv://{DB_USER}:{DB_PASSWORD}@{DB_HOST}?retryWrites=true&w=majority")
 
     db = client.salary_data
     collection = db.salary_data_coll
@@ -27,4 +35,4 @@ def get_data():
     return df
         
 if __name__ == '__main__':
-    get_data()
+    print(get_data())
