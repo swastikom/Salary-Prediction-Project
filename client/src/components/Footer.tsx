2bd8c84f5dc05f937fba4ff76cf56639c03af245
Swastik Dhar <swastikdhar18@gmail.com>
2023-04-21 00:12:33 +0530

    copyright section fixed

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -5,18 +5,27 @@ const styles = {
   footer: "justify-center align-center",
   footerContainer: "flex align-center justify-center items-center pt-5 pb-5",
   footerItems: "pl-5 pr-5 text-white",
-  footerBottom: "text-white justify-center pt-5 pb-5",
+  footerBottom: "text-white text-center text-xs justify-center pt-5 pb-5",
+  
 };
 
 const Footer = () => {
   return (
     <div className={styles.footer}>
       <ul className={styles.footerContainer}>
-        <li className={styles.footerItems}><Link href={''}>Home</Link> </li>
-        <li className={styles.footerItems}><Link href={''}>Home</Link> </li>
-        <li className={styles.footerItems}><Link href={''}>Home</Link> </li>
+        <li className={styles.footerItems}>
+          <Link href={""}>Home</Link>{" "}
+        </li>
+        <li className={styles.footerItems}>
+          <Link href={""}>Home</Link>{" "}
+        </li>
+        <li className={styles.footerItems}>
+          <Link href={""}>Home</Link>{" "}
+        </li>
       </ul>
-      <div className={styles.footerBottom}>Copyright</div>
+      <div className={styles.footerBottom}>
+        <Link href={""}>Copyright</Link>{" "}
+      </div>
     </div>
   );
 }
